ed0e3b52be7a95a03f404d67b39647cad3a4d33d
bill chipman <wrchipman@gmail.com>
2024-01-01 22:21:55 -0300

    Broke something

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -7,8 +7,10 @@ public static class Settings
     public const float fadeInSeconds = 0.25f;
     public const float fadeOutSeconds = 0.35f;
     public const float targetAlpha = 0.45f;
-    
-    
+
+    // Tilemap
+    public const float gridCellSize = 1f; // grid cell size in unity units
+
     //Player movements
     public const float runningSpeed = 5.333f;
     public const float walkingSpeed = 2.666f;
