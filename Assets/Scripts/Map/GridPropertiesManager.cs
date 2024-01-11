b6c4c669833135c3a9a75f9461ce8b1a7270479d
bill chipman <wrchipman@gmail.com>
2024-01-10 22:14:58 -0300

    implemented AStar pathfinding algorithm

--- Assets/Scripts/Map/GridPropertiesManager.cs
+++ Assets/Scripts/Map/GridPropertiesManager.cs
@@ -599,6 +599,29 @@ public class GridPropertiesManager : SingletonMonobehaviour<GridPropertiesManage
         return GetGridPropertyDetails(gridX, gridY, gridPropertyDictionary);
     }
 
+
+    public bool GetGridDimensions(SceneName sceneName, out Vector2Int gridDimensions, out Vector2Int gridOrigin)
+    {
+        gridDimensions = Vector2Int.zero;
+        gridOrigin = Vector2Int.zero;
+
+        //Loop through scenes
+        foreach (SO_GridProperties so_GridProperties in so_gridPropertiesArray)
+        {
+            if (so_GridProperties.sceneName == sceneName)
+            {
+                gridDimensions.x = so_GridProperties.gridWidth;
+                gridDimensions.y = so_GridProperties.gridHeight;
+
+                gridOrigin.x = so_GridProperties.originX;
+                gridOrigin.y = so_GridProperties.originY;
+
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void ISaveableDeregister()
     {
         SaveLoadManager.Instance.iSaveableObjectList.Remove(this);
